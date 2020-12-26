85069518b4136deeec5fd989b4b58dd5d5fe28e6
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-12-26 14:08:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let names=["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names){
+ for (let i = 0 ; i < names.length ; i++) {
+ 
+   console.log( 'Thank you, ${name[i]}  for the wonderful surprise gift!');
+}
+return names;
+}
+
+
+writeCards();
+
+
+function countDown(){
+let i=10;
+while (i<11){
+  console.log("${i}");
+  i--;
+}
+}
+countDown(10);
